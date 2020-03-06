390e2154cde063abc0e342f5eac058ae177be8c7
Ahmed Gamal <58369917+AG-Sign@users.noreply.github.com>
2020-03-06 18:52:40 +0200

    Add files via upload

---
+++ Lab3(iceHockey)/IPlayersFinder.java
@@ -0,0 +1,7 @@
+package com.company;
+//package eg.edu.alexu.csd.datastructure.iceHockey;
+//package eg.edu.alexu.csd.datastructure.iceHockey.cs6;
+
+public interface IPlayersFinder {
+    java.awt.Point[] findPlayers(String [] photo, int team , int threshold);
+}
