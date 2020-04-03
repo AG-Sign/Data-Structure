6d5f208136319eac0c049926c137e0956bd4707d
Ahmed Gamal <58369917+AG-Sign@users.noreply.github.com>
2020-04-03 10:06:00 +0200

    Add files via upload

---
+++ Lab 5/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.company","l":"Evaluate"},{"p":"com.company","l":"IExpressionEvaluator"},{"p":"com.company","l":"IStack"},{"p":"com.company","l":"Main"},{"p":"com.company","l":"Stacks.Node2"},{"p":"com.company","l":"Stacks"}]
